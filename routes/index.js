114f39b089edeecccdd92d1378110f495a7a2251
nugialindro <nugialindro@gmail.com>
2022-06-22 15:47:42 +0700

    Change edit path on router

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const articlesController = require("../controllers/articlesController");
 router.get("/articles", articlesController.index);
 router.get("/articles/:id", articlesController.show);
 router.post("/articles", articlesController.store);
-router.put("/articles/:id", articlesController.update);
+router.put("/articles/edit/:id", articlesController.update);
 router.delete("/articles/:id", articlesController.destroy);
 
 module.exports = router;
